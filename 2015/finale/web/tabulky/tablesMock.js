f816b03b335c52620e484a3c2c0472352d2dbc95
Jirka Kosek <jirka@kosek.cz>
2018-06-23 15:02:58 +0200

    Doplnění roku 2015

---
+++ 2015/finale/web/tabulky/tablesMock.js
@@ -0,0 +1,26 @@
+ var tables = {
+	
+	setTitle: function(title) {
+	},
+	
+	createTable: function(id) {
+	},
+	
+	setColumnCount: function(n) {
+	},
+	
+	setHeaderTitles: function(headerTitles) {
+	},
+	
+	setRowCount: function(n) {
+	},
+	
+	addRow: function() {		
+	},
+	
+	addRowFromString: function(rowString) {
+	},
+	
+	addRowsJson: function(json) {
+	}
+ }
